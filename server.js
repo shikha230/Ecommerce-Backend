ce4776be3699d137199437f012835ffe47065455
sikha230 <shikha230>
2026-06-12 18:03:27 +0530

    server modified

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5176",
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://veronaa.in/",
+  "https://veronaa.in",
 ];
 
 app.use(cors({
